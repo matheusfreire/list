90a603da1c8e9d8a3cfe23465b98d0daa71ae35b
Matheus Freire <matheus.sfreire@gmail.com>
2017-01-16 23:55:47 -0200

    First commit

---
+++ src/main/java/com/freire/controller/UsersController.java
@@ -0,0 +1,10 @@
+package com.freire.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/users")
+public class UsersController {
+
+}
---
+++ src/main/java/com/freire/model/User.java
@@ -0,0 +1,36 @@
+package com.freire.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="users")
+public class User implements Serializable{
+
+	private static final long serialVersionUID = -6172483943294206398L;
+	
+	@Id
+	@GeneratedValue(generator="users_seq",strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name="users_seq",sequenceName="USERS_SEQ", allocationSize=1)
+	@Getter @Setter
+	private long id;
+	
+	@Column(name="email")
+	@Getter @Setter
+	private String email;
+	
+	@Getter @Setter
+	private String password;
+	
+
+}
